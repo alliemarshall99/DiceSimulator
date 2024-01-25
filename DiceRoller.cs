1a85865dc2a3e71ca97c8e396d6e8865fe297465
User <User@DESKTOP-IOPJBN7>
2024-01-24 21:14:07 -0800

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class DiceRoller
+{
+    // simulate dice rolls and return results
+    public int[] SimulateDiceRolls(int numberOfRolls)
+    {
+        int[] results = new int[13];
+
+        // generate random numbers
+        Random random = new Random();
+
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            // roll  two dice
+            int dice1 = random.Next(1, 7);
+            int dice2 = random.Next(1, 7);
+            int sum = dice1 + dice2;
+
+            results[sum]++;
+        }
+
+        return results;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+// Allie Marshall
+// Section 3-3
+// January 17, 2024
+class DiceSimulator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+        Console.Write("How many dice rolls would you like to simulate? ");
+
+        // user inputs number of dice rolls
+        int numberOfRolls = int.Parse(Console.ReadLine());
+
+        // create an instance of the DiceRoller class
+        DiceRoller diceRoller = new DiceRoller();
+
+        // roll dice with results
+        int[] results = diceRoller.SimulateDiceRolls(numberOfRolls);
+
+        // display dice rolls
+        DisplayDice(results, numberOfRolls);
+
+        Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    static void DisplayDice(int[] results, int totalRolls)
+    {
+        Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {totalRolls}.");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = results[i] * 100 / totalRolls;
+            Console.WriteLine($"{i}: {new string('*', percentage)}");
+        }
+    }
+}
